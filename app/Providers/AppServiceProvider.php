dc966cda971192704516056c902be56afb4e8619
Julius Duff <8215455@ntc.edu.ph>
2024-12-03 21:54:21 +0800

    initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use App\View\Components\Navigation\Navigation;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Blade::component('navigation-navigation', Navigation::class);
+    }
+}
